2b0bc047cd51ea508106cda3d2972a81afb40ff9
muhammedRinshad210 <m.rinshad210@gmail.com>
2026-04-02 08:57:53 +0530

    debug true

--- buykopro/settings.py
+++ buykopro/settings.py
@@ -83,18 +83,10 @@ WSGI_APPLICATION = 'buykopro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'buyko',
-        'USER': 'root',
-        'PASSWORD': 'dilshad786',
-        'HOST': 'localhost',  
-        'PORT': '3306',        
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
